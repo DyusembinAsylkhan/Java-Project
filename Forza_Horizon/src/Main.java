e7082c0c6bc0aa10906bb89c80349978bd980ce7
Assylkhan_Motion <93905248+DyusembinAsylkhan@users.noreply.github.com>
2022-02-24 16:28:04 +0600

    Add files via upload

---
+++ Forza_Horizon/src/Main.java
@@ -0,0 +1,11 @@
+
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Java_Game"); //JFrame
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit
+        f.setSize(1100,600);
+        f.setVisible(true);
+
+    }
+}
